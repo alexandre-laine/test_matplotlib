aaf0bc58cc92d1af60947273717d0abaa8641161
alexandre-laine <118631107+alexandre-laine@users.noreply.github.com>
2024-10-28 09:16:44 +0100

    augmentation du nombre de point

--- gen_plot.py
+++ gen_plot.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import numpy as np
 import os
 
 cwd = os.getcwd()
-
-x = np.linspace(0, 100, 100001)
-y = np.random.random(size=x.shape[0])
 
 fig,ax = plt.subplots(
     nrows=1,
@@ -13,11 +11,18 @@ fig,ax = plt.subplots(
     figsize=(10,5)
 )
 
-ax.plot(
-    x,
-    y,
-    "--k"
-)
+for i in tqdm(range(10)):
+
+    x = np.linspace(0, 100, 100001)
+    y = np.random.random(size=x.shape[0])
+
+
+
+    ax.plot(
+        x,
+        y,
+        "--k"
+    )
 
 plt.tight_layout()
 fig.savefig(os.path.join(cwd, "test_plot.png"), dpi=200)
